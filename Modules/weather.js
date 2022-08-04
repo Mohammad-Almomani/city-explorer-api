bb7f466032654b8b83aba4a1dd2f72e05d31ae04
Mohammad-Almomani <moh.moma.mmm@gmail.com>
2022-08-04 18:18:13 +0300

    added timestamp

--- Modules/weather.js
+++ Modules/weather.js
@@ -1,13 +1,16 @@
 'use strict';
 const axios =require('axios');
-const weatherCache = {};
-
+const cache = require ('./cache');
 
 // reference for checking current date https://usefulangle.com/post/187/nodejs-get-date-time
 let dateFormat = new Date();
 
-let date = ("0" + dateFormat.getDate()).slice(-2), month = ("0" + (dateFormat.getMonth() + 1)).slice(-2), year = dateFormat.getFullYear();
-let currentDate = `${year}-${month}-${date}`;
+let date = ("0" + dateFormat.getDate()).slice(-2), month = ("0" + (dateFormat.getMonth() + 1)).slice(-2), year = dateFormat.getFullYear(),hours = dateFormat.getHours(),minutes = dateFormat.getMinutes();
+let currentDate = `${year}-${month}`;
+
+let dataSaveTime = `${year}-${month}-${date} ${hours}:${minutes}`;
+
+console.log(dataSaveTime);
 
 
 async function handleWeather(req, res) {
@@ -16,17 +19,18 @@ async function handleWeather(req, res) {
   let lat = req.query.lat;
   let lon = req.query.lon;
 
-  if (weatherCache[searchName] !==undefined && weatherCache[searchName][0].date.split(':')[0]===currentDate){
-    res.status(200).send(weatherCache[searchName]);
+  if (cache[`${searchName}-weather`] !==undefined && cache[`${searchName}-weather`][0].date.split(':')[0]===currentDate){
+    console.log(Date.now());
+    res.status(200).send(cache[`${searchName}-weather`]);
   } else {
 
     const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&include=minutely`;
     const weatherTEST = await axios.get(url);
     try {
       const dataArr = weatherTEST.data.data.map(day => new Forecast(day));
-      weatherCache[searchName] = dataArr;
-      console.log(`I am working`);
-      res.status(200).send(dataArr);
+      // cache[`${searchName}-weather`] = dataArr;
+      cache[`${searchName}-weather`]=dataArr;
+      res.status(200).send(cache[`${searchName}-weather`]);
     } catch (err) {
       res.status(500).send('City not found');
     }
@@ -40,6 +44,7 @@ class Forecast {
   constructor(day) {
     this.date = day.datetime;
     this.description = day.weather.description;
+    this.dataSaveDate = dataSaveTime;
   }
 }
 module.exports = handleWeather;
